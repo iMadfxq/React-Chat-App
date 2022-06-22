8caa9673758ba0b18730111e3a4c0ab42a80bd03
iMadfxq <imadfxq@gmail.com>
2022-06-21 22:09:02 -0400

    Created first component. Just starting

---
+++ src/Components/message-form/message-form.component.jsx
@@ -0,0 +1,12 @@
+import './message-form.styles.scss'
+
+export default function MessageForm() {
+  return (
+    <section className="message__form--container">
+      <form>
+        <input type="text" />
+        <button type='submit'></button>
+      </form>
+    </section>
+  )
+}
