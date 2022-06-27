1841bc727c7c0d337c7eed63fa331a8b2d931eb8
iMadfxq <imadfxq@gmail.com>
2022-06-26 20:48:25 -0400

    Finished basic design home route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import MessageForm from "./Components/message-form/message-form.component";
 import "./App.scss";
 import { Route, Routes } from "react-router";
+
+import Home from "./Routes/Home/home.route";
 
 function App() {
   return (
     <Routes>
-      <Route index element={<MessageForm />} />
-      <Route path="/p" element={} />
+      <Route index element={<Home />} />
     </Routes>
   );
 }
--- src/Routes/Home/home.route.jsx
+++ src/Routes/Home/home.route.jsx
@@ -1,1 +1,29 @@
 import './home.styles.scss'
+
+export default function Home() {
+  return (
+    <main className='main'>
+      <button>theme</button>
+      <h1>iMadfxq's Chat</h1>
+      <h2>Choose your channel</h2>
+      <section className="main__channelSelector">
+        <article className='main__channelSelector--general'>
+          <h3>General</h3>
+          <ul>
+            <li>Send a greeting</li>
+            <li>Ask how my journey has been</li>
+            <li>Tell people why you like cats so much</li>
+          </ul>
+        </article>
+        <article className='main__channelSelector--jobs'>
+          <h3>Jobs</h3>
+          <ul>
+            <li>Look for my portfolio</li>
+            <li>Find my resume</li>
+            <li>Contact me</li>
+          </ul>
+        </article>
+      </section>
+    </main>
+  )
+}
