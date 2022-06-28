653748fab383be434b87860bccb129743d23a9c9
iMadfxq <imadfxq@gmail.com>
2022-06-27 20:51:25 -0400

    Home design totally finished

--- src/Routes/Home/home.route.jsx
+++ src/Routes/Home/home.route.jsx
@@ -1,29 +1,33 @@
-import './home.styles.scss'
+import "./home.styles.scss";
 
 export default function Home() {
   return (
-    <main className='main'>
-      <button>theme</button>
-      <h1>iMadfxq's Chat</h1>
-      <h2>Choose your channel</h2>
+    <main className="main">
+      <button className="main__themeSelector">theme</button>
+      <section className="main__title">
+        <h1>iMadfxq's Chat</h1>
+        <h2>Choose your channel:</h2>
+      </section>
       <section className="main__channelSelector">
-        <article className='main__channelSelector--general'>
+        <article className="main__channelSelector--general">
           <h3>General</h3>
           <ul>
             <li>Send a greeting</li>
-            <li>Ask how my journey has been</li>
-            <li>Tell people why you like cats so much</li>
+            <li>Ask about my journey</li>
+            <li>Tell some jokes</li>
           </ul>
+          <button>Visit this chat</button>
         </article>
-        <article className='main__channelSelector--jobs'>
+        <article className="main__channelSelector--jobs">
           <h3>Jobs</h3>
           <ul>
             <li>Look for my portfolio</li>
             <li>Find my resume</li>
             <li>Contact me</li>
           </ul>
+          <button>Visit this chat</button>
         </article>
       </section>
     </main>
-  )
+  );
 }
