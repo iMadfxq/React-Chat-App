c97b551c9bdfb74543b60b37bb63bca4e6725e8a
iMadfxq <imadfxq@gmail.com>
2022-07-03 12:21:39 -0400

    Modal design finished

--- src/Components/setName-modal/setName-modal.component.jsx
+++ src/Components/setName-modal/setName-modal.component.jsx
@@ -3,15 +3,16 @@ import './setName-modal.styles.scss'
 export default function SetNameModal() {
   return (
     <section className="wrapper">
+      <span className='closeButton'>🆇</span>
       <section className="SetNameModal">
         <form>
           <label>
-            How do you wanna be called?
-            <input type="text" />
+            <span>How do you wanna be called?</span>
+            <input type="text" required/>
           </label>
           <button>Set Name</button>
         </form>
-        <p>I'll not let you change it later</p>
+        <p>**I'll not let you change it later**</p>
       </section>
     </section>
   )
