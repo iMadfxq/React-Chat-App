129d384db7b5c47d747c7c7ece6229ad21308cb8
iMadfxq <imadfxq@gmail.com>
2022-07-13 23:29:24 -0400

    Started creating the confirm no name modal

--- src/Components/setName-modal/setName-modal.component.jsx
+++ src/Components/setName-modal/setName-modal.component.jsx
@@ -1,23 +1,42 @@
-import './setName-modal.styles.scss'
+import "./setName-modal.styles.scss";
+
+import { useRef } from "react";
 
 export default function SetNameModal() {
 
-  const closeModal = (e) => {
-e.target.parentElement.classList.add('closed')
-  }
+  const confirmModal = useRef(null)
+  const setNameModal = useRef(null)
+
+
+  const closeSetNameModal = (e) => {
+    confirmModal.current.classList.add('open')
+  };
   return (
-    <section className="wrapper">
-      <span onClick={closeModal} className='closeButton'>🆇</span>
-      <section className="SetNameModal">
-        <form>
-          <label>
-            <span>How do you wanna be called?</span>
-            <input type="text" required/>
-          </label>
-          <button>Set Name</button>
-        </form>
-        <p>**I'll not let you change it later**</p>
+    <>
+      <section ref={setNameModal} className="wrapper">
+        <span onClick={closeSetNameModal} className="closeButton">
+          🆇
+        </span>
+        <section className="SetNameModal">
+          <form>
+            <label>
+              <span>How do you wanna be called?</span>
+              <input type="text" required />
+            </label>
+            <button>Set Name</button>
+          </form>
+          <p>**I'll not let you change it later**</p>
+        </section>
       </section>
-    </section>
-  )
+      <section className="confirm-closing" ref={confirmModal}>
+        <section className="confirm-modal">
+          <h3>I won't let you change your name later, are you sure?</h3>
+          <section>
+            <button>No</button>
+            <button>Yes</button>
+          </section>
+        </section>
+      </section>
+    </>
+  );
 }
