80f0b787e0b7eb11770dff2cf90703fcb282eedf
iMadfxq <imadfxq@gmail.com>
2022-07-15 22:42:06 -0400

    Making home link work

--- src/Components/channel-chat/channel-chat.component.jsx
+++ src/Components/channel-chat/channel-chat.component.jsx
@@ -2,13 +2,17 @@ import MessageForm from "../message-form/message-form.component"
 import ChannelMessages from "../channel-messages/channel-messages.component"
 
 import './channel-chat.styles.scss'
+import { useNavigate } from "react-router"
 
 export default function Channel({channelName}) {
+  const navigate = useNavigate()
   return (
     <section className="channel">
       <header className="channel__header">
         <h2>#{channelName}</h2>
-        <button>⌂ Home</button>
+        <button onClick={() => {
+          navigate('/')
+        }}>⌂ Home</button>
       </header>
       <ChannelMessages />
       <MessageForm />
