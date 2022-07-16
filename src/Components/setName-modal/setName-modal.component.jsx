89c2622f26f616da750ef03b312799958b20f81d
iMadfxq <imadfxq@gmail.com>
2022-07-15 20:46:57 -0400

    'Are you sure you don't want to set a name' screen completed

--- src/Components/setName-modal/setName-modal.component.jsx
+++ src/Components/setName-modal/setName-modal.component.jsx
@@ -1,16 +1,33 @@
 import "./setName-modal.styles.scss";
 
 import { useRef } from "react";
+import { useEffect } from "react";
 
 export default function SetNameModal() {
-
-  const confirmModal = useRef(null)
-  const setNameModal = useRef(null)
-
+  const confirmModal = useRef(null);
+  const setNameModal = useRef(null);
+  const nameInput = useRef(null)
 
   const closeSetNameModal = (e) => {
-    confirmModal.current.classList.add('open')
+    confirmModal.current.classList.add("open");
   };
+
+  const noNameHandleClick = (e) => {
+    e.stopPropagation();
+    if (e.target.textContent === "Yes") {
+      confirmModal.current.classList.remove("open");
+      confirmModal.current.classList.add("closed");
+      setNameModal.current.classList.add("closed");
+    }
+    if (e.target.textContent === "No") {
+      confirmModal.current.classList.remove("open");
+      confirmModal.current.classList.add("closed");
+      nameInput.current.focus()
+    }
+  };
+  useEffect(() => {
+    nameInput.current.focus()
+  }, [])
   return (
     <>
       <section ref={setNameModal} className="wrapper">
@@ -21,7 +38,7 @@ export default function SetNameModal() {
           <form>
             <label>
               <span>How do you wanna be called?</span>
-              <input type="text" required />
+              <input type="text" ref={nameInput} required />
             </label>
             <button>Set Name</button>
           </form>
@@ -32,8 +49,8 @@ export default function SetNameModal() {
         <section className="confirm-modal">
           <h3>I won't let you change your name later, are you sure?</h3>
           <section>
-            <button>No</button>
-            <button>Yes</button>
+            <button onClick={noNameHandleClick}>Yes</button>
+            <button onClick={noNameHandleClick}>No</button>
           </section>
         </section>
       </section>
